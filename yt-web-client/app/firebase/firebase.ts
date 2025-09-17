147b481604cfaf971699f9c8e86421abf16206f4
sruthisanjana <sanjanayadala12@gmail.com>
2025-09-17 18:07:37 -0500

    Add Firebase Auth with Google Sign-In/Sign-Out and update Navbar

---
+++ yt-web-client/app/firebase/firebase.ts
@@ -0,0 +1,48 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { 
+    getAuth,
+    signInWithPopup, 
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    User,
+
+
+} from "firebase/auth";
+import { get } from "http";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXLVxASPlfek9V5VfUkD4PGeWsIFdHKPg",
+  authDomain: "yt-clone-4f459.firebaseapp.com",
+  projectId: "yt-clone-4f459",
+  appId: "1:609076761842:web:d196e9c1da675107baac6b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+/**
+ * Sign the user in with a Google popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
+export function signInWithGoogle(){
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs the user out.
+ * @returns A promise that resolves with the user is signed out.
+ */
+export function signOut(){
+    return auth.signOut();
+}
+
+/**
+ * Trigger a callback when user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+export function onAuthStateChangedHelper(callback: (user: User | null) => void){
+    return onAuthStateChanged(auth, callback);
+}
